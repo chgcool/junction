cc923baa0ce6234a0375d8c491e95dabca0380e4
Slava <slava.perchuk@gmail.com>
2018-11-24 17:15:32 +0200

    idle waves

---
+++ Assets/Scripts/Tiling.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiling : MonoBehaviour
+{
+
+    public bool reverseScale = true;
+    private bool hasLeftBuddy = false;
+    private bool hasRightBuddy = false;
+
+    private float cameraHorizontalExtend;
+    private float spriteWidth;
+
+    // Use this for initialization
+    void Start () {
+        SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+        spriteWidth = sRenderer.sprite.bounds.size.x * System.Math.Abs(transform.localScale.x);
+
+        // orthographicSize is half of height of camera. Thus, horizontal extend can be calculated
+        cameraHorizontalExtend = Camera.main.orthographicSize * Screen.width / Screen.height;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (hasRightBuddy == false && transform.position.x + spriteWidth/2 < Camera.main.transform.position.x + cameraHorizontalExtend)
+        {
+            makeNewBuddy(1);
+            hasRightBuddy = true;
+        } else if (hasLeftBuddy == false && transform.position.x - spriteWidth/2 > Camera.main.transform.position.x - cameraHorizontalExtend)
+        {
+            makeNewBuddy(-1);
+            hasLeftBuddy = true;
+        }
+	}
+
+    private void makeNewBuddy(int leftOrRight)
+    {
+        Transform newBuddy = Instantiate(transform);
+        newBuddy.parent = transform.parent;
+
+        newBuddy.position = new Vector3(transform.position.x + spriteWidth * leftOrRight - leftOrRight, transform.position.y, transform.position.z);
+
+        // scaling for smooth connection of tiles
+        if (reverseScale)
+        {
+            newBuddy.localScale = new Vector3(newBuddy.localScale.x*-1, newBuddy.localScale.y, newBuddy.localScale.z);
+        }
+
+        // set buddy parameters of new object
+        if (leftOrRight == 1)
+        {
+            newBuddy.GetComponent<Tiling>().hasLeftBuddy = true;
+        }
+        else
+        {
+            newBuddy.GetComponent<Tiling>().hasRightBuddy = true;
+        }
+    }
+}
